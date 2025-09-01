a97b68349d44c7f03918f22aa94ebfdd0138f4de
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-01 14:16:55 +0200

    home button

--- src/features/chat/ChatHome.jsx
+++ src/features/chat/ChatHome.jsx
@@ -3,7 +3,7 @@ import ChatInput from "./ChatInput";
 function ChatHome() {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold mb-4">Welcome to the Chat App</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome to Assistant</h1>
       <div className="w-full text-center text-gray-600 dark:text-gray-400 mb-6">
         <ChatInput />
       </div>
--- src/features/menu/MenuHeader.jsx
+++ src/features/menu/MenuHeader.jsx
@@ -1,9 +1,9 @@
-import { FaAtom } from "react-icons/fa6";
+import { FaSeedling } from "react-icons/fa6";
 
 function MenuHeader({ action }) {
   return (
     <div className="cursor-pointer flex mx-2 my-2 px-3 py-2" onClick={action}>
-      <FaAtom className="size-6" />
+      <FaSeedling className="size-5" />
     </div>
   );
 }
