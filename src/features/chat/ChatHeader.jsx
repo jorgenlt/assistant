a62b26f61e32d83d04f151ab9bfec1492e39e060
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-02 21:40:24 +0200

    chat header

--- src/features/chat/ChatHeader.jsx
+++ src/features/chat/ChatHeader.jsx
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux";
+import { FaChevronDown } from "react-icons/fa6";
+
 function ChatHeader() {
+  const { current, openAi, anthropic, mistral } = useSelector(
+    (state) => state.providers
+  );
+
+  const providers = [openAi.name, anthropic.name, mistral.name];
+  console.log("🚀 ~ providers:", providers);
+
   return (
-    <div className="flex-shrink-0">
-      <p>Chat Header</p>
+    <div className="border-b border-[#83838326]">
+      <div className="cursor-pointer select-none rounded-xl hover:bg-[var(--hover)] w-fit flex items-center py-2 my-2 mx-2 px-4">
+        <span className="mr-2">
+          {current.name} ({current.model})
+        </span>
+        <FaChevronDown size={13} />
+      </div>
     </div>
   );
 }
