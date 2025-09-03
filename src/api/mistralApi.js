9ffab7888d717d8a019ade3325f6ab32039281e9
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-03 13:04:01 +0200

    mistral api

--- src/api/mistralApi.js
+++ src/api/mistralApi.js
@@ -1,21 +1,27 @@
-import MistralClient from "@mistralai/mistralai";
+import { Mistral } from "@mistralai/mistralai";
+import systemPrompt from "./systemPrompt";
 
 const fetchMistralChatCompletion = async (context, prompt, providers) => {
   const { key: API_KEY, model: MODEL } = providers.mistral;
+
+  const systemMessage = {
+    role: "system",
+    content: systemPrompt, // This uses your imported string
+  };
 
   const userMessage = {
     role: "user",
     content: prompt,
   };
 
-  const messages = [...context, userMessage];
+  const messages = [systemMessage, ...context, userMessage];
 
-  const client = new MistralClient(API_KEY);
+  const client = new Mistral({ apiKey: API_KEY });
 
   try {
-    const chatResponse = await client.chat({
+    const chatResponse = await client.chat.complete({
       model: MODEL,
-      messages: messages,
+      messages,
     });
 
     const { content, role } = chatResponse.choices[0].message;
@@ -27,7 +33,7 @@ const fetchMistralChatCompletion = async (context, prompt, providers) => {
   } catch (error) {
     console.error(
       "Error in fetchMistralChatCompletion:",
-      error.message || error.response.data.error?.message
+      error.message || error.response?.data?.error?.message
     );
     throw error;
   }
