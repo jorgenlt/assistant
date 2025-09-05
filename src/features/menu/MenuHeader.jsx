6b505b4d255b1c002c2a1a08a2e36cf2430b1874
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-05 14:26:51 +0200

    favicon

--- src/features/menu/MenuHeader.jsx
+++ src/features/menu/MenuHeader.jsx
@@ -1,4 +1,4 @@
-import { FaSeedling, FaAnglesLeft } from "react-icons/fa6";
+import { FaWorm, FaAnglesLeft } from "react-icons/fa6";
 
 function MenuHeader({ action }) {
   return (
@@ -7,7 +7,7 @@ function MenuHeader({ action }) {
         className="mx-3 px-2 py-2 cursor-pointer w-fit rounded-xl hover:bg-[var(--hover)]"
         onClick={action}
       >
-        <FaSeedling className="size-5" color="#ee29f5" />
+        <FaWorm className="size-5" color="#ee29f5" />
       </div>
       <div
         className="hidden mx-3 px-2 py-2 cursor-pointer w-fit rounded-xl hover:bg-[var(--hover)]"
