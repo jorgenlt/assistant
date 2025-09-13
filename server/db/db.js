0d81e6c2a0171c0129f7061ccf12b3b198f8f777
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-13 12:17:42 +0200

    connected to mongodb atlas

--- server/db/db.js
+++ server/db/db.js
@@ -2,15 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose
-      // Local connection
-      .connect(process.env.MONGO_URI, {});
-    // MongoDB Atlas
-    // .connect(process.env.MONGO_URL, {});
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("MongoDB connected successfully.");
   } catch (error) {
     console.error(`${error} did not connect`);
-    // Exit process with failure
     process.exit(1);
   }
 };
--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 // Defining routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/conversations", conversationRoutes)
+app.use("/conversations", conversationRoutes);
 
 // Default Route Handler
 app.use((req, res, next) => {
