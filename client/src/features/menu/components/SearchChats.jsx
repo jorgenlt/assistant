5d47211d2dad5221fe1001480431aa6ffe3c357f
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-18 11:07:55 +0200

    search chats component

--- client/src/features/menu/components/SearchChats.jsx
+++ client/src/features/menu/components/SearchChats.jsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
+
 const SearchChats = () => {
+  const [query, setQuery] = useState("");
+
   return (
     <div>
-      <p>Search chats</p>
+      <div className="border-b-1 border-b-gray-700 py-2">
+        <input autoFocus value={query} onChange={(e) => setQuery(e.value)} />
+      </div>
+      <div className="py-2 h-[40vh] overflow-y-scroll overflow-x-hidden">
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+        <div className="p-2 rounded-xl hover:bg-[var(--hover)]">
+          <p>... result ...</p>
+        </div>
+      </div>
     </div>
   );
 };
