b6e5ead0b97a5db761a890013247434fe2477421
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-29 13:09:03 +0200

    update config

--- client/src/app/config.js
+++ client/src/app/config.js
@@ -1,3 +1,5 @@
 // export const BASE_API_URL = 'http://localhost:3001';
-export const BASE_API_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://assistant-a64w.onrender.com";
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const serverUrl = "https://assistant-a64w.onrender.com";
+
+export const BASE_API_URL = VITE_API_BASE_URL || serverUrl;
