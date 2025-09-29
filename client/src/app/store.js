2ee1d0c8a0ce3b0424a62f2de539fe2b21353ad8
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-09-29 18:29:24 +0200

    configure whitelist redux store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -23,7 +23,7 @@ const persistConfig = {
   key: "root",
   version: 1,
   storage,
-  // whitelist: ["files", "status", "error"],
+  whitelist: ["menu", "providers", "auth"],
 };
 
 // Combine reducers
