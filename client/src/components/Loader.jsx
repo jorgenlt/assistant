f80dac3b0195467f5bf34819a897348b0b902d3e
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-10-01 19:35:51 +0200

    change loader type

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { ClipLoader } from "react-spinners";
+import { SpinnerCircularFixed } from "spinners-react";
 
 const Loader = ({ isThemeDark, text }) => {
   const [showText, setShowText] = useState(false);
@@ -13,7 +13,13 @@ const Loader = ({ isThemeDark, text }) => {
 
   return (
     <div className="flex flex-col justify-center items-center h-full w-full gap-8">
-      <ClipLoader size={80} color={isThemeDark ? "#fafafa" : "#121416"} />
+      <SpinnerCircularFixed
+        size={80}
+        thickness={60}
+        speed={110}
+        color={isThemeDark ? "#fafafa" : "#121416"}
+        secondaryColor="rgba(0, 0, 0, 0.4)"
+      />
       <div className="h-4">
         {text && showText && (
           <span className="inline-block animate-pulse text-lg">{text}</span>
