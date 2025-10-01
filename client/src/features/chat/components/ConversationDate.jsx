2b6f6d6e0683e3d1ae0beaee4c12873c71be88af
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-10-01 19:44:59 +0200

    select none, conv. date

--- client/src/features/chat/components/ConversationDate.jsx
+++ client/src/features/chat/components/ConversationDate.jsx
@@ -1,6 +1,6 @@
 const ConversationDate = ({ created }) => {
   return (
-    <div className="w-full text-center mt-20 mb-10">
+    <div className="w-full text-center mt-20 mb-10 select-none">
       <span className="text-gray-400">{created}</span>
     </div>
   );
