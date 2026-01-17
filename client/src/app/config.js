679c8ca98d4ff07ef573232807da774205325768
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2026-01-17 15:42:51 +0800

    update serverUrl

--- client/src/app/config.js
+++ client/src/app/config.js
@@ -1,6 +1,6 @@
 // export const BASE_API_URL = 'http://localhost:3001';
 const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const serverUrl = "https://ishaan.hidencloud.com:24609";
+const serverUrl = "https://api.assistant.jorgenlt.no";
 
 export const BASE_API_URL = VITE_API_BASE_URL || serverUrl;
 
