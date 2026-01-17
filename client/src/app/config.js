e86b76cdc5ed44973cf92316cc7ff51f4ad39302
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2026-01-17 22:23:33 +0800

    update serverUrl

--- client/src/app/config.js
+++ client/src/app/config.js
@@ -1,6 +1,6 @@
 // export const BASE_API_URL = 'http://localhost:3001';
 const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const serverUrl = "https://api.assistant.jorgenlt.no";
+const serverUrl = "https://api-assistant.jorgenlt.no";
 
 export const BASE_API_URL = VITE_API_BASE_URL || serverUrl;
 
